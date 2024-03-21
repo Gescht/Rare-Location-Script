863d926ba2989376618d8035bdfa41f914a058a3
Gescht <tobias.alfons.tampe@gmail.com>
2024-03-21 19:22:35 +0100

    change syntax of lua file
    - creature_type value put into quotes (symbol)
    - respawn value put into quotes (symbol)
    - elite value set to lower case

--- Python Web Scraper/convert-rar-dict-to-lua.py
+++ Python Web Scraper/convert-rar-dict-to-lua.py
@@ -15,16 +15,20 @@ with open("rareSpawnData.lua", "w") as the_file:
             the_file.write("\t\t\t[\""+str(rareName)+"\"] = {")
             
             for key, value in rareData.items():
+                #handle location data
                 if type(value) is list:
                     the_file.write(key+" = {")
                     for element in value:
                         the_file.write(element+",")
                     the_file.write("},")
+                #handle elite
+                elif type(value) is bool:
+                    the_file.write(key+"="+str(value).lower()+",")
+                #handle respawn or creature type
+                elif "respawn" == key or "creature_type" == key:
+                    the_file.write(key+"=\""+value+"\",")
                 else:
-                    if type(value) is bool:
-                        the_file.write(key+"="+str(value)+",")
-                    else:
-                        the_file.write(key+"="+value+",")
+                    the_file.write(key+"="+value+",")
             the_file.write("},\n")
 
         the_file.write("\t\t},\n")
