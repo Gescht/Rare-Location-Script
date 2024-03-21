e76002187729f931849bef0498b852e5b2c3d57f
Gescht <tobias.alfons.tampe@gmail.com>
2024-03-21 23:57:37 +0100

    adjust order of operation

--- Python Web Scraper/convert-rar-dict-to-lua.py
+++ Python Web Scraper/convert-rar-dict-to-lua.py
@@ -15,18 +15,18 @@ with open("rareSpawnData.lua", "w") as the_file:
             the_file.write("\t\t\t[\""+str(rareName)+"\"] = {")
             
             for key, value in rareData.items():
-                #handle location data
-                if type(value) is list:
-                    the_file.write(key+" = {")
-                    for element in value:
-                        the_file.write(element+",")
-                    the_file.write("},")
                 #handle elite
-                elif type(value) is bool:
+                if type(value) is bool:
                     the_file.write(key+"="+str(value).lower()+",")
                 #handle respawn or creature type
                 elif "respawn" == key or "creature_type" == key:
                     the_file.write(key+"=\""+value+"\",")
+                #handle location data
+                elif type(value) is list:
+                    the_file.write(key+" = {")
+                    for element in value:
+                        the_file.write(element+",")
+                    the_file.write("},")
                 else:
                     the_file.write(key+"="+value+",")
             the_file.write("},\n")
